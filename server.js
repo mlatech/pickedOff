3000cc5d5c56bb5eb3b8e3f7f060fa26d4c1079f
Karyna Chernyak <mlatech@icloud.com>
2023-05-25 06:38:59 -0500

    .

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require("express")
 const app = express()
-const {v4: uuidv4} = require('uuid')
+var middleware = require("./routes/middleware.js")
 
+
+app.use("/search", middleware)
+
+//get request 
+app.get("/search", (req, res) => {
+    req.body = {Date: "'Thursday May 25, 2023"}
+    res.send(req.body)
+    
+    })
+
+    app.listen(9000, ()=>{
+        console.log("Server is running on port 9000")
+    })
